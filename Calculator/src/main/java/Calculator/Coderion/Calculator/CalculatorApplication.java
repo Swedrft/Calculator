64a3d7872ee5dbb61d2cca25c90a6102c1b7c960
User <adam.holdys522@gmail.com>
2025-03-03 14:36:24 +0100

    Add files via upload

--- Calculator/src/main/java/Calculator/Coderion/Calculator/CalculatorApplication.java
+++ Calculator/src/main/java/Calculator/Coderion/Calculator/CalculatorApplication.java
@@ -1,28 +1,61 @@
 package Calculator.Coderion.Calculator;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Scanner;
 
 @SpringBootApplication
-public class CalculatorApplication {
+public class CalculatorApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(CalculatorApplication.class, args);
-		int Numer_raty = 1;
-		int ilosc_rat = 18;
-		double wysokosc_raty =203.95;
-		double wynik;
-		if (Numer_raty>ilosc_rat){
-			wynik = 0;
-			System.out.println(wynik);
-		}else System.out.println(wysokosc_raty);
-
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Scanner scanner = new Scanner(System.in);
 
+		System.out.println("Wpisz kwote:");
+		while (!scanner.hasNextInt()) {
+			System.out.println("To nie jest poprawna liczba. Wpisz kwote:");
+			scanner.next();
+		}
+		int kwota = scanner.nextInt();
+		scanner.nextLine();
 
+		System.out.println("Wpisz ilosc rat:");
+		while (!scanner.hasNextInt()) {
+			System.out.println("To nie jest poprawna liczba. Wpisz ilosc rat:");
+			scanner.next();
+		}
+		int ilosc_rat = scanner.nextInt();
+		scanner.nextLine();
 
+		System.out.println("Wpisz oprocentowanie roczne:");
+		while (!scanner.hasNextInt()) {
+			System.out.println("To nie jest poprawna liczba. Wpisz oprocentowanie roczne:");
+			scanner.next();
+		}
+		int oprocentowanie_roczne = scanner.nextInt();
+		scanner.nextLine();
 
+		System.out.println("liczba miesiecznych rat:");
+		while (!scanner.hasNextInt()) {
+			System.out.println("To nie jest poprawna liczba. Wpisz liczba miesiecznych rat:");
+			scanner.next();
+		}
+		int liczba_rat_miesiac = scanner.nextInt();
+		scanner.nextLine();
 
+		System.out.println("Kwota: " + kwota);
+		System.out.println("Ilosc rat: " + ilosc_rat);
+		System.out.println("Oprocentowanie roczne: " + oprocentowanie_roczne + "%");
+		System.out.println("Liczba rat: " + liczba_rat_miesiac);
+
+		double oprocentowanie_miesieczne = (double) oprocentowanie_roczne / 12 / 100;
+		double Rata_kredytu = kwota * oprocentowanie_miesieczne * Math.pow(1 + oprocentowanie_miesieczne, liczba_rat_miesiac) /
+				(Math.pow(1 + oprocentowanie_miesieczne, liczba_rat_miesiac) - 1);
+		System.out.println("Rata kredytu: " + Rata_kredytu);
+	}
 }
