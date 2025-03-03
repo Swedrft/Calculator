1db480f2059e7450c2a62798f824221f24a95030
Swedrft <104517601+swedrft@users.noreply.github.com>
2025-03-03 10:25:28 +0100

    Add files via upload

--- Calculator/src/main/java/Calculator/Coderion/Calculator/CalculatorApplication.java
+++ Calculator/src/main/java/Calculator/Coderion/Calculator/CalculatorApplication.java
@@ -7,7 +7,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CalculatorApplication.class, args);
+		int Numer_raty = 1;
+		int ilosc_rat = 18;
+		double wysokosc_raty =203.95;
+		double wynik;
+		if (Numer_raty>ilosc_rat){
+			wynik = 0;
+			System.out.println(wynik);
+		}else System.out.println(wysokosc_raty);
+
 	}
+
+
+
+
+
 
 }
