3473c506c948d5fe10f8caefd570a3e735369ccf
User <adam.holdys522@gmail.com>
2025-03-04 11:11:52 +0100

    Add files via upload

--- src/main/java/Calculator/Coderion/Calculator/CalculatorApplication.java
+++ src/main/java/Calculator/Coderion/Calculator/CalculatorApplication.java
@@ -14,6 +14,44 @@ public class CalculatorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Wpisz kwote:");
+		while (!scanner.hasNextInt()) {
+			System.out.println("To nie jest poprawna liczba. Wpisz kwote:");
+			scanner.next();
+		}
+		int kwota = scanner.nextInt();
+		scanner.nextLine();
+
+		System.out.println("Wpisz ilosc rat:");
+		while (!scanner.hasNextInt()) {
+			System.out.println("To nie jest poprawna liczba. Wpisz ilosc rat:");
+			scanner.next();
+		}
+		int ilosc_rat = scanner.nextInt();
+		scanner.nextLine();
+
+		System.out.println("Wpisz oprocentowanie roczne:");
+		while (!scanner.hasNextInt()) {
+			System.out.println("To nie jest poprawna liczba. Wpisz oprocentowanie roczne:");
+			scanner.next();
+		}
+		int oprocentowanie_roczne = scanner.nextInt();
+		scanner.nextLine();
+
+		System.out.println("liczba miesiecznych rat:");
+		while (!scanner.hasNextInt()) {
+			System.out.println("To nie jest poprawna liczba. Wpisz liczba miesiecznych rat:");
+			scanner.next();
+		}
+		int liczba_rat_miesiac = scanner.nextInt();
+		scanner.nextLine();
+
+		System.out.println("Kwota: " + kwota);
+		System.out.println("Ilosc rat: " + ilosc_rat);
+		System.out.println("Oprocentowanie roczne: " + oprocentowanie_roczne + "%");
+		System.out.println("Liczba rat miesiecznie: " + liczba_rat_miesiac);
 	CalculatorService CalculatorService= new CalculatorService();
 	CalculatorService.calculate();
 	}
