116f4a6bf3f43e13d7a03b8c8f4e47a767609076
Swedrft <104517601+Swedrft@users.noreply.github.com>
2025-03-05 13:10:43 +0100

    Revert "poprawiona logika kalkulator"

--- src/main/java/Calculator/Coderion/Calculator/CalculatorService.java
+++ src/main/java/Calculator/Coderion/Calculator/CalculatorService.java
@@ -5,10 +5,11 @@ import java.math.RoundingMode;
 public class CalculatorService {
     public BigDecimal rataKredytu;
 
-    public BigDecimal calculate(int kwota, int liczba_rat,int suma_odsetek) {
+    public BigDecimal calculate(int kwota, int liczba_rat) {
         int oprocentowanie_roczne = 23;
         double oprocentowanie_miesieczne = (double) oprocentowanie_roczne / 12 / 100;
-        double rata_kredytu = (double) (kwota + suma_odsetek) /liczba_rat;
+        double rata_kredytu = kwota * oprocentowanie_miesieczne * Math.pow(1 + oprocentowanie_miesieczne, liczba_rat) /
+                (Math.pow(1 + oprocentowanie_miesieczne, liczba_rat) - 1);
         rataKredytu = BigDecimal.valueOf(rata_kredytu).setScale(2, RoundingMode.HALF_UP);
         return rataKredytu;
     }
