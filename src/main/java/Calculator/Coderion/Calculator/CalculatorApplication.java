e4593ad2926063bd37b26f9234ec852cea667417
User <adam.holdys522@gmail.com>
2025-03-05 14:47:02 +0100

    dodanie Raty kapitałowej

--- src/main/java/Calculator/Coderion/Calculator/CalculatorApplication.java
+++ src/main/java/Calculator/Coderion/Calculator/CalculatorApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Date;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -19,13 +21,6 @@ public class CalculatorApplication implements CommandLineRunner {
 		Scanner scanner = new Scanner(System.in);
 		int suma_odsetek= (int) 571.04;
 
-		System.out.println("Wpisz Datę kredytu:");
-		while (!scanner.hasNextInt()) {
-			System.out.println("To nie jest poprawna Data. Wpisz datę:");
-			scanner.next();
-		}
-		int Data = scanner.nextInt();
-		scanner.next();
 
 		System.out.println("Wpisz kwote:");
 		while (!scanner.hasNextInt()) {
@@ -43,16 +38,20 @@ public class CalculatorApplication implements CommandLineRunner {
 		int liczba_rat = scanner.nextInt();
 		scanner.nextLine();
 
+		int Rata_kapitalowa_techniczna = (int) kwota / liczba_rat;
+		BigDecimal zaokraglona_Rata_kapitalowa = BigDecimal.valueOf(Rata_kapitalowa_techniczna).setScale(2, RoundingMode.HALF_UP);
+
 
 		System.out.println("Kwota: " + kwota);
 		System.out.println("Ilosc rat: " + liczba_rat);
 		System.out.println("Oprocentowanie roczne: " + 23 + "%");
 		System.out.println( "Suma odsetek: " +suma_odsetek+ "zl" );
-		System.out.println("Data kredytu: "+Data);
+		System.out.println("Rata Kapitalowa techniczna: " +zaokraglona_Rata_kapitalowa);
+
 	CalculatorService CalculatorService= new CalculatorService();
 
 
 
-		System.out.println("Rata kredytu: " + CalculatorService.calculate(kwota,liczba_rat,suma_odsetek));
+		System.out.println("Rata kredytu: " + CalculatorService.calculate(kwota,liczba_rat,suma_odsetek,Rata_kapitalowa_techniczna));
 	}
 }
