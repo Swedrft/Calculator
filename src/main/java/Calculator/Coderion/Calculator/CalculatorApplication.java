0f56044ae7af86ea6914d6a5106bf1dc04d2dbb4
User <adam.holdys522@gmail.com>
2025-03-06 14:32:02 +0100

    Harmonogram do Kalkulatora

--- src/main/java/Calculator/Coderion/Calculator/CalculatorApplication.java
+++ src/main/java/Calculator/Coderion/Calculator/CalculatorApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Date;
+import java.time.temporal.ChronoUnit;
+import java.time.LocalDate;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -19,8 +21,15 @@ public class CalculatorApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		Scanner scanner = new Scanner(System.in);
+
 		int suma_odsetek= (int) 571.04;
 
+		int Karencja_w_miesiacach = 0;
+
+		LocalDate data = LocalDate.of(2025,3,6);
+		int rok = data.getYear();
+		int miesiac = data.getMonthValue();
+		int dzien = data.getDayOfMonth();
 
 		System.out.println("Wpisz kwote:");
 		while (!scanner.hasNextInt()) {
@@ -42,16 +51,41 @@ public class CalculatorApplication implements CommandLineRunner {
 		BigDecimal zaokraglona_Rata_kapitalowa = BigDecimal.valueOf(Rata_kapitalowa_techniczna).setScale(2, RoundingMode.HALF_UP);
 
 
+
+
 		System.out.println("Kwota: " + kwota);
 		System.out.println("Ilosc rat: " + liczba_rat);
 		System.out.println("Oprocentowanie roczne: " + 23 + "%");
 		System.out.println( "Suma odsetek: " +suma_odsetek+ "zl" );
 		System.out.println("Rata Kapitalowa techniczna: " +zaokraglona_Rata_kapitalowa);
 
+
+
+
 	CalculatorService CalculatorService= new CalculatorService();
 
 
 
 		System.out.println("Rata kredytu: " + CalculatorService.calculate(kwota,liczba_rat,suma_odsetek,Rata_kapitalowa_techniczna));
+		for (int i = 1; i <= liczba_rat; i++) {
+			LocalDate wynikowaData;
+
+
+			if (data.getDayOfMonth() >= 21) {
+				wynikowaData = data.plusMonths(2 + Karencja_w_miesiacach).withDayOfMonth(10);
+			} else {
+				wynikowaData = data.plusMonths(1).withDayOfMonth(10);
+			}
+
+
+			long liczba_dni_na_splate = ChronoUnit.DAYS.between(data, wynikowaData);
+
+
+			data = wynikowaData;
+
+
+			System.out.println("Liczba rat: " + i + "  | Termin platnosci: " + wynikowaData + "  | Liczba dni na splate: " + liczba_dni_na_splate);
+		}
+
 	}
 }
--- src/main/java/Calculator/Coderion/Calculator/CalculatorService.java
+++ src/main/java/Calculator/Coderion/Calculator/CalculatorService.java
@@ -1,20 +1,20 @@
 package Calculator.Coderion.Calculator;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class CalculatorService {
     public BigDecimal rataKredytu;
 
-    public BigDecimal calculate(int kwota, int liczba_rat,int suma_odsetek,int Rata_kapitalowa_techniczna) {
+    public BigDecimal calculate(int kwota, int liczba_rat, int suma_odsetek, double Rata_kapitalowa_techniczna) {
         int oprocentowanie_roczne = 23;
-        Rata_kapitalowa_techniczna = (int) kwota /liczba_rat ;
+        Rata_kapitalowa_techniczna = (double) kwota / liczba_rat;
         double oprocentowanie_miesieczne = (double) oprocentowanie_roczne / 12 / 100;
-        double rata_kredytu = (double) (kwota + suma_odsetek) /liczba_rat;
+        double rata_kredytu = (double) (kwota + suma_odsetek) / liczba_rat;
+
         rataKredytu = BigDecimal.valueOf(rata_kredytu).setScale(2, RoundingMode.HALF_UP);
         return rataKredytu;
     }
 
 
-
 }
-
