5ef79754beb6beb49ded3dec1ff77fb82b622097
User <adam.holdys522@gmail.com>
2025-03-07 14:14:07 +0100

    poprawki test

--- src/main/java/Calculator/Coderion/Calculator/CalculatorService.java
+++ src/main/java/Calculator/Coderion/Calculator/CalculatorService.java
@@ -1,7 +1,5 @@
 package Calculator.Coderion.Calculator;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
@@ -9,34 +7,19 @@ import java.util.List;
 
 public class CalculatorService {
 
-    public BigDecimal rataKredytu;
-    private static final int KARENCJA_W_MIESIACACH = 0;
-    private static final  int oprocentowanie_roczne = 23;
-    private static final  int dzien_splaty_raty=10;
-    private static final  int InstallmentTreshold = 21;
-
     public List<Rata> calculate(int kwota, int liczba_rat, LocalDate dataPoczatkowa) {
-
-        double oprocentowanie_miesieczne = (double) oprocentowanie_roczne / 12 / 100;
-
         List<Rata> raty = new ArrayList<>();
         LocalDate data = dataPoczatkowa;
 
+
         for (int i = 1; i <= liczba_rat; i++) {
-            LocalDate wynikowaData;
+            LocalDate wynikowaData = data.plusMonths(1).withDayOfMonth(10);
+            long liczbaDniNaSplate = ChronoUnit.DAYS.between(data, wynikowaData);
 
-            if (data.getDayOfMonth() >= 21) {
-                wynikowaData = data.plusMonths(2 + KARENCJA_W_MIESIACACH).withDayOfMonth(10);
-            } else {
-                wynikowaData = data.plusMonths(1).withDayOfMonth(10);
-            }
 
-            long liczba_dni_na_splate = ChronoUnit.DAYS.between(data, wynikowaData);
-
-            Rata rata = new Rata(i, wynikowaData);
+            Rata rata = new Rata(i, wynikowaData, liczbaDniNaSplate);
             raty.add(rata);
 
-            System.out.println("Liczba rat: " + i + " | Liczba dni na splate: " + liczba_dni_na_splate);
 
             data = wynikowaData;
         }
--- src/test/java/Calculator/Coderion/Calculator/CalculatorServiceTest.java
+++ src/test/java/Calculator/Coderion/Calculator/CalculatorServiceTest.java
@@ -1,8 +1,8 @@
 package Calculator.Coderion.Calculator;
 
 import org.junit.jupiter.api.Test;
+
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,7 +14,7 @@ public class CalculatorServiceTest {
 
         int kwota = 5000;
         int liczba_rat = 6;
-        LocalDate dataPoczatkowa = LocalDate.parse("2025-03-07");
+        LocalDate dataPoczatkowa = LocalDate.of(2025, 3, 7);
 
 
         CalculatorService calculatorService = new CalculatorService();
@@ -23,28 +23,35 @@ public class CalculatorServiceTest {
         List<Rata> raty = calculatorService.calculate(kwota, liczba_rat, dataPoczatkowa);
 
 
-        assertNotNull(raty, "Lista rat nie może być nullem");
+        assertNotNull(raty, "Lista rat nie moze być nullem");
         assertEquals(liczba_rat, raty.size(), "Liczba rat powinna być zgodna");
+
 
         assertEquals(1, raty.get(0).getNumerRaty());
         assertEquals(LocalDate.of(2025, 4, 10), raty.get(0).getTerminPlatnosci());
+        assertEquals(34, raty.get(0).getIloscDni());
 
         assertEquals(2, raty.get(1).getNumerRaty());
-        assertEquals(LocalDate.of(2025, 5, 10), raty.get(0).getTerminPlatnosci());
+        assertEquals(LocalDate.of(2025, 5, 10), raty.get(1).getTerminPlatnosci());
+        assertEquals(30, raty.get(1).getIloscDni());
 
         assertEquals(3, raty.get(2).getNumerRaty());
-        assertEquals(LocalDate.of(2025, 6, 10), raty.get(0).getTerminPlatnosci());
-
+        assertEquals(LocalDate.of(2025, 6, 10), raty.get(2).getTerminPlatnosci());
+        assertEquals(31, raty.get(2).getIloscDni());
         assertEquals(4, raty.get(3).getNumerRaty());
-        assertEquals(LocalDate.of(2025, 7, 10), raty.get(0).getTerminPlatnosci());
+        assertEquals(LocalDate.of(2025, 7, 10), raty.get(3).getTerminPlatnosci());
+        assertEquals(30, raty.get(3).getIloscDni());
 
         assertEquals(5, raty.get(4).getNumerRaty());
-        assertEquals(LocalDate.of(2025, 8, 10), raty.get(0).getTerminPlatnosci());
+        assertEquals(LocalDate.of(2025, 8, 10), raty.get(4).getTerminPlatnosci());
+        assertEquals(31, raty.get(4).getIloscDni());
 
         assertEquals(6, raty.get(5).getNumerRaty());
-        assertEquals(LocalDate.of(2025, 9, 10), raty.get(0).getTerminPlatnosci());
+        assertEquals(LocalDate.of(2025, 9, 10), raty.get(5).getTerminPlatnosci());
+        assertEquals(31, raty.get(5).getIloscDni());
+
+
 
 
     }
-    }
-
+}
