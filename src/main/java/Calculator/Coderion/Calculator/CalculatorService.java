7f3a2a4e04a2793e962ebead1d9adaa6d1fd679d
User <adam.holdys522@gmail.com>
2025-03-07 15:06:21 +0100

    poprawki test i dodanie nowego testu

--- src/main/java/Calculator/Coderion/Calculator/CalculatorService.java
+++ src/main/java/Calculator/Coderion/Calculator/CalculatorService.java
@@ -11,19 +11,27 @@ public class CalculatorService {
         List<Rata> raty = new ArrayList<>();
         LocalDate data = dataPoczatkowa;
 
-
+        // Pętla dla każdej raty
         for (int i = 1; i <= liczba_rat; i++) {
-            LocalDate wynikowaData = data.plusMonths(1).withDayOfMonth(10);
+            LocalDate wynikowaData;
+
+            if (data.getDayOfMonth() >= 21) {
+                wynikowaData = data.plusMonths(2).withDayOfMonth(10);
+            } else {
+                wynikowaData = data.plusMonths(1).withDayOfMonth(10);
+            }
+
             long liczbaDniNaSplate = ChronoUnit.DAYS.between(data, wynikowaData);
 
-
+            // Tworzymy nową ratę i dodajemy do listy
             Rata rata = new Rata(i, wynikowaData, liczbaDniNaSplate);
             raty.add(rata);
 
-
+            // Aktualizujemy "data" na kolejną datę płatności
             data = wynikowaData;
         }
 
         return raty;
     }
+
 }
--- src/test/java/Calculator/Coderion/Calculator/CalculatorServiceTest.java
+++ src/test/java/Calculator/Coderion/Calculator/CalculatorServiceTest.java
@@ -49,9 +49,50 @@ public class CalculatorServiceTest {
         assertEquals(6, raty.get(5).getNumerRaty());
         assertEquals(LocalDate.of(2025, 9, 10), raty.get(5).getTerminPlatnosci());
         assertEquals(31, raty.get(5).getIloscDni());
-
-
-
-
     }
+    @Test
+    public void testCalculatePoDniu21() {
+
+        int kwota = 5000;
+        int liczba_rat = 6;
+        LocalDate dataPoczatkowa = LocalDate.of(2025, 3, 22);
+
+        CalculatorService calculatorService = new CalculatorService();
+
+
+        List<Rata> raty = calculatorService.calculate(kwota, liczba_rat, dataPoczatkowa);
+
+
+        assertNotNull(raty, "Lista rat nie może być nullem");
+        assertEquals(liczba_rat, raty.size(), "Liczba rat powinna być zgodna");
+
+
+        assertEquals(1, raty.get(0).getNumerRaty());
+        assertEquals(LocalDate.of(2025, 5, 10), raty.get(0).getTerminPlatnosci());
+        assertEquals(49, raty.get(0).getIloscDni());
+
+        assertEquals(2, raty.get(1).getNumerRaty());
+        assertEquals(LocalDate.of(2025, 6, 10), raty.get(1).getTerminPlatnosci());
+        assertEquals(31, raty.get(1).getIloscDni());
+
+        assertEquals(3, raty.get(2).getNumerRaty());
+        assertEquals(LocalDate.of(2025, 7, 10), raty.get(2).getTerminPlatnosci());
+        assertEquals(30, raty.get(2).getIloscDni());
+
+        assertEquals(4, raty.get(3).getNumerRaty());
+        assertEquals(LocalDate.of(2025, 8, 10), raty.get(3).getTerminPlatnosci());
+        assertEquals(31, raty.get(3).getIloscDni());
+
+        assertEquals(5, raty.get(4).getNumerRaty());
+        assertEquals(LocalDate.of(2025, 9, 10), raty.get(4).getTerminPlatnosci());
+        assertEquals(31, raty.get(4).getIloscDni());
+
+        assertEquals(6, raty.get(5).getNumerRaty());
+        assertEquals(LocalDate.of(2025, 10, 10), raty.get(5).getTerminPlatnosci());
+        assertEquals(30, raty.get(5).getIloscDni());
+    }
+
+
 }
+
+
