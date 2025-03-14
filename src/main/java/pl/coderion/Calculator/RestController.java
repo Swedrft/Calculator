507eaa9a2bcd3c8011125a5d17a440f8c6f15a6e
User <adam.holdys522@gmail.com>
2025-03-14 11:38:32 +0100

    dodanie Kontrolera

--- src/main/java/pl/coderion/Calculator/RestController.java
+++ src/main/java/pl/coderion/Calculator/RestController.java
@@ -1,24 +1,36 @@
 package pl.coderion.Calculator;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+class CalculationRequest {
+    private BigDecimal kwota;
+    private int liczbaRat;
+    private LocalDate dataPoczatkowa;
+}
+
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping(value = "/api/calculate")
 public class RestController {
 
-    private final CalculatorService calculatorService;
+    private CalculatorService calculatorService;
 
-    @Autowired
     public RestController(CalculatorService calculatorService) {
         this.calculatorService = calculatorService;
     }
 
-    public Oferta calculate(@RequestBody CalculationRequest request) {
 
+
+    @PostMapping
+    public Oferta calculate(@RequestBody CalculationRequest request) {
         return calculatorService.calculate(
                 request.getKwota(),
                 request.getLiczbaRat(),
@@ -27,32 +39,4 @@ public class RestController {
     }
 }
 
-class CalculationRequest {
-    private BigDecimal kwota;
-    private int liczbaRat;
-    private LocalDate dataPoczatkowa;
 
-    public BigDecimal getKwota() {
-        return kwota;
-    }
-
-    public void setKwota(BigDecimal kwota) {
-        this.kwota = kwota;
-    }
-
-    public int getLiczbaRat() {
-        return liczbaRat;
-    }
-
-    public void setLiczbaRat(int liczbaRat) {
-        this.liczbaRat = liczbaRat;
-    }
-
-    public LocalDate getDataPoczatkowa() {
-        return dataPoczatkowa;
-    }
-
-    public void setDataPoczatkowa(LocalDate dataPoczatkowa) {
-        this.dataPoczatkowa = dataPoczatkowa;
-    }
-}
