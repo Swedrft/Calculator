79aaf1f6ddd04c65d1758c92e5f0d20ccfcc0665
User <adam.holdys522@gmail.com>
2025-03-19 12:14:22 +0100

    poprawka i dodanie klasy CalculationRequest

---
+++ src/main/java/pl/coderion/Calculator/CalculationRequest.java
@@ -0,0 +1,15 @@
+package pl.coderion.Calculator;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CalculationRequest {
+    private BigDecimal kwota;
+    private int liczbaRat;
+    private LocalDate dataPoczatkowa;
+}
