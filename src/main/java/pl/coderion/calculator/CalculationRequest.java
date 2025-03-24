ddbd8e2e893ef9c1cdcfd20b9c1762306461ee28
User <adam.holdys522@gmail.com>
2025-03-24 10:38:44 +0100

    odsetki-harmonogram-dodanie nowego testu

--- src/main/java/pl/coderion/calculator/CalculationRequest.java
+++ src/main/java/pl/coderion/calculator/CalculationRequest.java
@@ -4,15 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import jakarta.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class CalculationRequest {
+
+    @NotNull
+    @DecimalMin(value = "0.1",message = "kwota musi być wieksza od zera")
     private BigDecimal kwota;
+
+    @Min(value = 1,message = "Liczba rat nie moze być zerem" )
     private int liczbaRat;
+
+    @NotNull(message = "data poczatkowa nie moze być pusta")
     private LocalDate dataPoczatkowa;
 
 }
